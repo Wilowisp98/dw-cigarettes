95eb0f253c25fdb9cc72254f26830ea5850dd6a5
duartepinto555 <49456938+duartepinto555@users.noreply.github.com>
2024-03-12 22:44:00 +0000

    Added coments to code

--- code/data_treatment.py
+++ code/data_treatment.py
@@ -1,16 +1,24 @@
 import pandas as pd
 
-file_name = '../datasets/acp-r1-r12-2016-2022-v1.6.csv'
+# Obtaining current working directory (necessary for when running with another folder as "Project" in VSCode)
+current_directory = __file__.split("\\")[:-1]
+file_name = f'{current_directory}/../datasets/acp-r1-r12-2016-2022-v1.6.csv'
 
-
+# -------------
+# Loading Data
+# -------------
 df = pd.read_csv(file_name, encoding='latin-1', low_memory=False)
 
-# Drop everything which has a incorrect Year
-df = df.query('Year >= 1000')
-df = df.dropna(subset=['Year', 'Month', 'Day'])
-
+# -------------
+# Treating Data
+# -------------
+df = df.query('Year >= 1000')   # Drop everything which has a incorrect Year
+df = df.dropna(subset=['Year', 'Month', 'Day'])      # Drop null values
+# Make date a datetime column
 df['Date'] = df['Year'].astype(int).astype(str) + '-' + df['Month'].astype(int).astype(str) + '-'  + df['Day'].astype(int).astype(str)
 df['Date'] = pd.to_datetime(df['Date'])
 
-
-df.to_csv('../datasets/cigarettes_treated.csv', index=False)
+# -------------
+# Writing Data
+# -------------
+df.to_csv(f'{current_directory}/../datasets/cigarettes_treated.csv', index=False)
