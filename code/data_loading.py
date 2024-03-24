3263511aeff8dc78742700a41faf0252a7e520a0
joao-carvalho <joao.david.17@gmail.com>
2024-03-24 22:34:52 +0000

    Created Time table

--- code/data_loading.py
+++ code/data_loading.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('datasets/cigarettes_treated.csv')
 
+# -----------------------------------------------
+# ALL OF THIS CAN GO TO THE DATA_TREATMENT SCRIPT
+# -----------------------------------------------
+
 # Creating Province IDs
 df['Province_ID'] = df['Province'].fillna('no_province')
 province_ids = {province_id: index for (index, province_id) in enumerate(df['Province_ID'].unique())}
@@ -16,6 +20,21 @@ df['City_ID'] = df['City_ID'].map(city_ids)
 df['Country_ID'] = df['Country'].fillna('no_country')
 country_ids = {country_id: index for (index, country_id) in enumerate(df['Country_ID'].unique())}
 df['Country_ID'] = df['Country_ID'].map(country_ids)
+
+# Creating Month Name Columns
+month_names = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
+               7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+df['Month_Name'] = df['Month'].map(month_names)
+
+# Creating Month IDs
+df['Month_ID'] = pd.factorize(df['Month'].astype(str) + '_' + df['Year'].astype(str))[0]
+
+# Creating Year IDs
+df['Year_ID'] = df['Year'].fillna('no_year')
+year_ids = {year_id: index for (index, year_id) in enumerate(df['Year_ID'].unique())}
+df['Year_ID'] = df['Year_ID'].map(year_ids)
+
 
 # -------------------------------------------------------------
 # STORES TABLE
@@ -53,6 +72,27 @@ for row in range(len(df_t)):
     sql_queries.append(ins)
 
 with open('localization.sql', 'w') as sql_file:
+
+    for query in sql_queries:
+        sql_file.write(query + '\n')
+
+# -------------------------------------------------------------
+# TIME TABLE
+
+df_sorted = df.sort_values(by=['Year', 'Month', 'Day'])
+columns = ['Day', 'Month_ID', 'Month_Name', 'Year_ID', 'Year']
+df_t = df_sorted[columns]
+df_t = df_t.drop_duplicates().reset_index(drop=True)
+
+sql_queries = [
+    "CREATE TABLE IF NOT EXISTS TIME (Day_ID INT AUTO_INCREMENT, Day INT, Month_ID INT, Month VARCHAR(20), Year_ID INT, Year INT, PRIMARY KEY(Day_ID));"
+]
+
+for row in range(len(df_t)):
+    ins = f'INSERT INTO TIME (Day, Month_ID, Month, Year_ID, Year) VALUES ({df_t["Day"][row]}, {df_t["Month_ID"][row]},"{df_t["Month_Name"][row]}", {df_t["Year_ID"][row]},{df_t["Year"][row]});'
+    sql_queries.append(ins)
+
+with open('time.sql', 'w') as sql_file:
 
     for query in sql_queries:
         sql_file.write(query + '\n')
