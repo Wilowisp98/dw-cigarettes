e9bfa0fc4be2c5f121887615773ea6d7ef1872d6
duartepinto555 <duarte.pinto7337@gmail.com>
2024-03-26 16:15:29 +0100

    Fixed Syntax in ETL

--- sql_queries/08-etl_dw.sql
+++ sql_queries/08-etl_dw.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:484a671f646d512f78a2fd76fd88ed1bda65e297ca867c894fddc0bfc4c85d06
-size 986
+oid sha256:6311ed84e51213b1c46043c6d556d6f7859b98933a6878fa813d48275d1f479e
+size 978
