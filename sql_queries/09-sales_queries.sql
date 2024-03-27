4221d67119737316e93403f4fbd8b002fc5db668
joao-carvalho <joao.david.17@gmail.com>
2024-03-27 16:07:53 +0000

    added simple rollup query

--- sql_queries/09-sales_queries.sql
+++ sql_queries/09-sales_queries.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5994686f37839c03629e45918ae713d61c4eeabee96f040fc0d547f8a07c482d
-size 2453
+oid sha256:e407d6b19e29a4d44aedb5b966bd6108ef8138b4229d953eb02c1a04137babd4
+size 2639
