69cf7bea257026c70afb34d45defbd248f591e0f
joao-carvalho <joao.david.17@gmail.com>
2024-03-27 14:09:57 +0000

    More queries

--- sql_queries/09-sales_queries.sql
+++ sql_queries/09-sales_queries.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f3a743cca1c283deb0d65afc1968642c514554329651de17cdce561b34d05e9
-size 294
+oid sha256:12cfc28b51e49d7655c89df195393a754c352d2ddd23810f694c113e2d7bb6e4
+size 664
