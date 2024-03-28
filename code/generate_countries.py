d38bb33d9cc1a1479123e78a5aca0ddee715b5c4
BrunoSilva <119140240+brunomfsilva@users.noreply.github.com>
2024-03-28 22:15:23 +0000

    add comments

--- code/generate_countries.py
+++ code/generate_countries.py
@@ -4,6 +4,7 @@ import os
 def main():
     current_directory = os.getcwd()
 
+    
     countries_ds = f'{current_directory}/datasets/b0bab1c0-d1c7-485c-b639-ee59bc2293f3_Data.csv'
     main_dataset = f'{current_directory}/datasets/cigarettes_treated.feather'
 
@@ -26,6 +27,7 @@ def main():
 
     uniques = df_main['Country'].unique()
 
+    
     df_filtered = df[df['Country Name'].isin(uniques)]
 
     df_filtered = df_filtered.rename(columns={'Population ages 15-64, female': '15_64_female'}) 
@@ -38,11 +40,14 @@ def main():
 
     df_filtered = df_filtered.rename_axis(None, axis=1)
 
-
+    #create countries df
     merged_df = pd.merge(df_filtered, df_main[['Country', 'Country_ID']], left_on='Country Name', right_on='Country', how='left')
     merged_df.drop_duplicates(subset=['Country Name', 'Country Code', 'Year'], inplace=True)
+    
+    #remove duplicates
     merged_df.drop('Country', axis=1, inplace=True)
 
+    # Put Country_ID on first column
     merged_df = merged_df[['Country_ID', 'Country Name', 'Country Code', 'Year', 
                            '15_64_female', '15_64_male', 'above_64_female', 'above_64_male', 
                            'pop_female', 'pop_male', 'total_pop']]
