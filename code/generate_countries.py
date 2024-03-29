b5264bf3de0a931dcc1cecc0c3efa4081c426873
Hugo <70277389+Wilowisp98@users.noreply.github.com>
2024-03-29 07:02:32 +0000

    comments

--- code/generate_countries.py
+++ code/generate_countries.py
@@ -1,13 +1,12 @@
 import pandas as pd
-import os
 import utils
 
 
 @utils.log_wrapper
 def main():
     current_directory = '\\'.join(__file__.split("\\")[:-1])
-
     
+    # Load datasets
     countries_ds = f'{current_directory}/../datasets/b0bab1c0-d1c7-485c-b639-ee59bc2293f3_Data.csv'
     main_dataset = f'{current_directory}/../datasets/cigarettes_treated.feather'
     dim_time = f'{current_directory}/../datasets/dim_time.feather'
@@ -16,14 +15,17 @@ def main():
     df_main = pd.read_feather(main_dataset)
     df_time = pd.read_feather(dim_time)
 
+    # Drop Series Code
     df.drop('Series Code', axis=1, inplace=True)
 
+    # Made column Year from the columns 2016...2022
     df = pd.melt(df, 
                  id_vars=['Country Name', 'Country Code', 'Series Name'], 
                  value_vars=['2016', '2017', '2018','2019', '2020', '2021', '2022'], 
                  var_name='Year', 
                  value_name='Value')
 
+    # Made new columns based on the values of the Series Name column
     df = df.pivot(index=['Country Name', 'Country Code', 'Year'], 
                   columns='Series Name',
                   values='Value')
@@ -35,6 +37,7 @@ def main():
     
     df_filtered = df[df['Country Name'].isin(uniques)]
 
+    # Rename columns
     df_filtered = df_filtered.rename(columns={'Population ages 15-64, female': '15_64_female'}) 
     df_filtered = df_filtered.rename(columns={'Population ages 15-64, male': '15_64_male'}) 
     df_filtered = df_filtered.rename(columns={'Population ages 65 and above, female': 'above_64_female'}) 
