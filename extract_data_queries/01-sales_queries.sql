6607b469bbe73a579e70e295ceb6b3ee906b5827
joao-carvalho <joao.david.17@gmail.com>
2024-04-02 18:01:18 +0100

    Fixed margin query

--- extract_data_queries/01-sales_queries.sql
+++ extract_data_queries/01-sales_queries.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf1a64ebaf489e874866815af51194851f83b1a7de176dfd55301a80db918f0e
-size 4795
+oid sha256:e19cd2cf2d927bc9b024bef5a78953fc30f26d5c5c0bcaec564bf81bf826ede8
+size 4821
