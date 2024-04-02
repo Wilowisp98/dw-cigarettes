a60fd6671f8282177d184dde2c29ac72211df403
joao-carvalho <joao.david.17@gmail.com>
2024-04-02 17:30:14 +0100

    Consuption per capita + Margin queries

--- extract_data_queries/01-sales_queries.sql
+++ extract_data_queries/01-sales_queries.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad85d410346fcd394d27427ea75212be3251ccc56942420a89018b5cb2fb1e97
-size 3461
+oid sha256:bf1a64ebaf489e874866815af51194851f83b1a7de176dfd55301a80db918f0e
+size 4795
